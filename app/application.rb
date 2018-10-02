53fcf358109d0b762d16718f543f24353f7d714d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-02 20:51:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+    
+    item = req.path.split("/songs/").last 
+    song = @@songs.find{|s| s.title == song_title}
+      
     if req.path=="/songs"
       resp.write "You requested the songs"
     else
