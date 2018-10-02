badb575647cf682ef5b9f0ec84597b97bdac1d14
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-02 21:09:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,7 @@ class Application
       
     if req.path.match(/items/)
       name = req.path.split("/items/").last 
-      if item = @@items.find { |i| i.name == name }
-        resp.write item.price
+      if item = @@items.find { |i| resp.write i.price }
       else
         resp.write = "Item not found"
         resp.status = 400
