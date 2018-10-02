3b828632add83da9bb1d71e51c41bc197eb641a5
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-02 21:11:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,6 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-      
     if req.path.match(/items/)
       name = req.path.split("/items/").last 
       if item = @@items.find { |i| i.name = name }
